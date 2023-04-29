7c26ab4010543add0e0406a3a4edc3ad565f0e6a
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-04-29 12:01:38 +0300

    task1

--- src/src/js/task-01.js
+++ src/src/js/task-01.js
@@ -15,10 +15,14 @@
 //Category: Technologies
 //Elements: 5
 const list = document.querySelector('#categories')
-const item = list.querySelectorAll('.item')
+const item = list.querySelectorAll('li.item')
 console.log(item)
-const elem1 = item.forEach((elem) => { 
-    
+const elem1 = item.forEach((elem) => {
+    const title = elem.firstElementChild
+    console.log(title)
+    const elements = elem.lastElementChild.querySelectorAll('li')
+    console.log(elements)
+   // return title, elements
 });
-console.log(elem1)
+//console.log(elem1)
 
