b1038c740f2f32610726a9dae9f13f276ed4340e
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-04-30 22:59:07 +0300

    task9

--- src/src/js/task-09.js
+++ src/src/js/task-09.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+const body = document.body;
+
+const changeColor = () => {
+  textColor.textContent = getRandomHexColor();
+  body.style.backgroundColor = textColor.textContent;
+  console.log(textColor.textContent)
+};
+
+changeColorBtn.addEventListener("click", changeColor);
