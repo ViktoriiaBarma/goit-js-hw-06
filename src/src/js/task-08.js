2ebaedf6cc46da43c985cfa0e1a4c64397bd5b5b
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-04-30 17:22:00 +0300

    task8

--- src/src/js/task-08.js
+++ src/src/js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form')
+form.addEventListener('submit', submitForm)
+const data = {}
 
+function submitForm(event) {
+  event.preventDefault();
+
+  const email = event.target.elements.email;
+  const password = event.target.elements.password;
+
+  if (email.value === "" || password.value === "") {
+    alert("Please enter login or password");
+  } else {
+    data.email = email.value;
+    data.password = password.value;
+    console.log(data);
+  }
+  event.target.reset();
+}
+
+  
